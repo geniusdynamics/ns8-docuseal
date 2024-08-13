93fa330b7773eb6836e9255bc3a0f36518104b9a
Martin Bhuong <martin@genius.ke>
2024-08-13 08:29:17 +0200

    Update to version 1.6.8

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="docuseal"
-docuseal_version="1.6.7"
+docuseal_version="1.6.8"
 # Create a new empty container image
 container=$(buildah from scratch)
 
